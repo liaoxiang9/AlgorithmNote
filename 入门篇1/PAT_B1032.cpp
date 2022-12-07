a5678927c211d9aaaaaa2edad1ae8266133689ab
liaoxiang9 <lx1500692043@aliyun.com>
2022-12-07 11:05:00 +0800

    2022-12-7

--- 入门篇1/PAT_B1032.cpp
+++ 入门篇1/PAT_B1032.cpp
@@ -1,3 +1,29 @@
 #include <cstdio>
 
+const int maxn = 100010;
 // 挖掘机技术哪家强
+int main(){
+    int N=0;
+    scanf("%d", &N);
+    int school[maxn]={0};
+    // for (int i = 0; i < N+5; i++)
+    // {
+    //     school[i] = 0;
+    // }
+    
+    int num=0, score=0;
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d %d", &num, &score);
+        school[num]+=score;
+    }
+    int max = -1;
+    int max_num = 1;
+    for(int i=1;i<N;i++){
+        if(school[i]>max){
+            max = school[i];
+            max_num = i;
+        }
+    }
+    printf("%d %d\n", max_num, max);
+}
